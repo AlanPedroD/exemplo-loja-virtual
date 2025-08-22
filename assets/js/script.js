35da767bae5c0c466e1a6f2031931ee977462d8b
Alan Dias <alandiaslp@hotmail.com>
2025-08-22 19:03:13 -0300

    precos editados

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,13 @@ let cart = [];
 // Produtos
 const products = [
   { id: "1", nome: "Calça Fashion", preco: 99.90, img: "assets/imagens/calca-fem-01.jpg" },
-  { id: "2", nome: "Calça Jeans", preco: 149.90, img: "assets/imagens/calca-jeans-02.jpg" },
-  { id: "3", nome: "Calca Casual", preco: 199.90, img: "assets/imagens/calca-casual.jpg" },
-  { id: "4", nome: "Calca Casual nv", preco: 249.90, img: "assets/imagens/calca-casual-02.jpg" },
-  { id: "5", nome: "Vestido Fashion", preco: 179.90, img: "assets/imagens/vestido-01.jpg" },
-  { id: "6", nome: "Conjunto Academia", preco: 179.90, img: "assets/imagens/conj-academia-01.jpg" },
-  { id: "7", nome: "Conjunto Academia nv", preco: 179.90, img: "assets/imagens/conj-academia-04.jpg" },
-  { id: "8", nome: "Conjunto praia", preco: 179.90, img: "assets/imagens/moda-praia-01.jpg" }
+  { id: "2", nome: "Calça Jeans", preco: 73.90, img: "assets/imagens/calca-jeans-02.jpg" },
+  { id: "3", nome: "Calca Casual", preco: 85.90, img: "assets/imagens/calca-casual.jpg" },
+  { id: "4", nome: "Calca Casual nv", preco: 79.90, img: "assets/imagens/calca-casual-02.jpg" },
+  { id: "5", nome: "Vestido Fashion", preco: 69.90, img: "assets/imagens/vestido-01.jpg" },
+  { id: "6", nome: "Conjunto Academia", preco: 74.90, img: "assets/imagens/conj-academia-01.jpg" },
+  { id: "7", nome: "Conjunto Academia nv", preco: 65.90, img: "assets/imagens/conj-academia-04.jpg" },
+  { id: "8", nome: "Conjunto praia", preco: 59.99, img: "assets/imagens/moda-praia-01.jpg" }
 ];
 
 // Renderizar produtos
